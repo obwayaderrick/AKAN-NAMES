54ff246aba24c3425fccb369b872a3dcc25c1e01
obwayaderrick <obwayaderrick@gmail.com>
2020-05-11 11:34:07 +0300

    made some changes

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ var male = ['kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 var Female =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 
 function dayofweek(d){
-    (d) = (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )% 7;
+    (d) = (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
     return d;
 };
